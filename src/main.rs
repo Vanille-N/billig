62c9653db79a371a6a9c41fb93ffd870eff1b618
Vanille-N <neven.villani@gmail.com>
2021-04-22 14:27:16 +0200

    update main

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,23 @@ mod lib;
 
 fn main() {
     let filename = std::env::args().nth(1).unwrap_or_else(|| "data.bil".to_string());
-    let contents = std::fs::read_to_string(&filename).expect("File not found");
 
+    let (entries, errs) = read_entries(&filename);
+    println!("{}", errs);
+
+}
+
+fn read_entries(filename: &str) -> (Option<Vec<(lib::date::Date, lib::entry::Entry)>>, lib::error::ErrorRecord) {
+    let contents = std::fs::read_to_string(&filename).expect("File not found");
     let mut errs = lib::error::ErrorRecord::new();
-    let data = {
-        let data = lib::parse::extract(&filename, &mut errs, &contents);
-        if errs.is_fatal() {
-            println!("{}", errs);
-            panic!();
-        }
-        data
-    };
-    let pairs = {
-        let pairs = lib::template::instanciate(&mut errs, data);
-        if errs.is_fatal()  {
-            println!("{}", errs);
-            panic!();
-        } else if errs.count_warnings() > 0 {
-            println!("{}", errs);
-        }
-        pairs
-    };
-    println!("{:#?}", pairs);
+    let data = lib::parse::extract(&filename, &mut errs, &contents);
+    if errs.is_fatal() {
+        return (None, errs);
+    }
+    let pairs = lib::template::instanciate(&mut errs, data);  
+    if errs.is_fatal()  {
+        (None, errs)
+    } else {
+        (Some(pairs), errs)
+    }
 }
