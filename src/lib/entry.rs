c19655c7ec2f0f01b570d363c8b1d5e1141861db
Vanille-N <neven.villani@gmail.com>
2021-04-24 17:35:59 +0200

    period

--- src/lib/entry.rs
+++ src/lib/entry.rs
@@ -1,5 +1,7 @@
 use std::fmt;
 
+use crate::lib::date::Date;
+
 pub mod fields {
     pub use super::{
         Amount,
@@ -112,6 +114,41 @@ impl Entry {
 impl Span {
     pub fn from(duration: Duration, window: Window, count: usize) -> Self {
         Self { duration, window, count }
+    }
+
+    pub fn period(&self, dt: Date) -> (Date, Date) {
+        use Duration::*;
+        use Window::*;
+        let nb = self.count as isize;
+        match (self.duration, self.window) {
+            (Day, Precedent) => (dt.jump_day(nb), dt.prev()),
+            (Day, Successor) => (dt.next(), dt.jump_day(nb)),
+            (Day, Anterior) => (dt.jump_day(-nb).next(), dt),
+            (Day, _) => (dt, dt.jump_day(nb).prev()),
+            (Week, Current) => (dt.start_of_week(), dt.end_of_week().jump_day(7 * (nb - 1))),
+            (Week, Anterior) => (dt.jump_day(-7 * nb).next(), dt),
+            (Week, Posterior) => (dt, dt.jump_day(7 * nb).prev()),
+            (Week, Precedent) => {
+                let d = dt.start_of_week();
+                (d.jump_day(-7 * nb), d.prev())
+            }
+            (Week, Successor) => {
+                let d = dt.end_of_week();
+                (d.next(), d.jump_day(7 * nb))
+            }
+            (Month, Current) => (dt.start_of_month(), dt.end_of_month().jump_month(nb - 1)),
+            (Month, Posterior) => (dt, dt.jump_month(nb).prev()),
+            (Month, Anterior) => (dt.jump_month(-nb).next(), dt),
+            (Month, Precedent) => {
+                let d = dt.start_of_month();
+                (d.jump_month(-nb), d.prev())
+            }
+            (Month, Successor) => {
+                let d = dt.start_of_month();
+                (d.jump_month(-nb), d.prev())
+            }
+            (Year, Current) => (dt.start_of_year(), dt.end_of_year().jump_year(nb - 1)),
+        }
     }
 }
 
