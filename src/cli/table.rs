797c97ea658bed542e4bd51c246b8bc44c68d3f0
Vanille-N <neven.villani@gmail.com>
2021-05-02 15:23:30 +0200

    formatters for better alignment

--- src/cli/table.rs
+++ src/cli/table.rs
@@ -10,29 +10,50 @@ pub struct Table<'d> {
     data: &'d [Summary],
 }
 
+struct BoxFmt {
+    width: usize,
+    text: String,
+}
+
+struct ColFmt {
+    width: usize,
+    label: BoxFmt,
+    boxes: Vec<BoxFmt>,
+}
+
+struct GridFmt {
+    labels: ColFmt,
+    columns: Vec<ColFmt>,
+}
+
 impl<'d> Table<'d> {
     pub fn from(data: &'d [Summary]) -> Self {
         Self { data }
+    }
+
+    fn to_formatter(&self) -> GridFmt {
+        let columns = (0..Category::COUNT)
+            .map(|i| Category::from_usize(i).unwrap())
+            .collect::<Vec<_>>();
+        let cols = columns
+            .iter()
+            .map(|c| BoxFmt::category(*c))
+            .map(|b| ColFmt::with_label(b))
+            .collect::<Vec<_>>();
+        let mut grid = GridFmt::with_columns(cols);
+        for sum in self.data {
+            grid.push_line(BoxFmt::period(sum.period()), sum.amounts().iter().map(|f| BoxFmt::amount(*f)).collect::<Vec<_>>());
+        }
+        grid
     }
 }
 
 
 impl fmt::Display for Table<'_> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let columns = (0..Category::COUNT)
-            .map(|i| Category::from_usize(i).unwrap())
-            .collect::<Vec<_>>();
-        for sum in self.data {
-            let pp = format!("{}", sum.period());
-            write!(f, "| {:>15} |", pp)?;
-            for cat in &columns {
-                let amount = sum.query(*cat);
-                let pp = if amount.nonzero() {
-                    format!("{}", sum.query(*cat))
-                } else {
-                    String::new()
-                };
-                write!(f, " {:>10} |", pp)?;
+        write!(f, "{}", self.to_formatter())
+    }
+}
             }
             writeln!(f);
         }
